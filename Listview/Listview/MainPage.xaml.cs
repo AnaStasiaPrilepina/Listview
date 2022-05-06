735204e86b9e8d0f2efb35fb8751053c2b4b7af2
AnaStasia <Ana@DESKTOP-U7R1815>
2022-05-06 09:03:11 +0300

    ruhm

--- Listview/Listview/MainPage.xaml.cs
+++ Listview/Listview/MainPage.xaml.cs
@@ -13,7 +13,8 @@ namespace Listview
     {
         //public string[] phones { get; set; }
         //public List<Telefon> telefons { get; set; }
-        public ObservableCollection<Telefon> telefons { get; set; }
+        //public ObservableCollection<Telefon> telefons { get; set; }
+        public ObservableCollection<Ruhm<string, Telefon>> telefonideruhmades { get; set; }
         ListView list;
         Label lbl;
         Button lisa, kustuta;
@@ -29,24 +30,69 @@ namespace Listview
             //    new Telefon {Nimetus = "Xiaomi Note 10S pro", Tootja = "Xiaomi", Hind = "389", Pilt = "xiaomis.jfif"},
             //};
 
-            telefons = new ObservableCollection<Telefon>
+            //telefons = new ObservableCollection<Telefon>
+            //{
+            //    new Telefon {Nimetus = "Samsung Galaxy S22 Ultra", Tootja = "Samsung", Hind = "1349", Pilt = "samsung.jpg"},
+            //    new Telefon {Nimetus = "Xiaomi Mi 11 Lite 5G", Tootja = "Xiaomi", Hind = "399", Pilt = "xiaomi.jfif"},
+            //    new Telefon {Nimetus = "iPhone 13", Tootja = "Apple", Hind = "1179", Pilt = "iphone.jfif"},
+            //    new Telefon {Nimetus = "Xiaomi Note 10S pro", Tootja = "Xiaomi", Hind = "389", Pilt = "xiaomis.jfif"},
+            //};
+
+            var telefonid = new List<Telefon>
             {
                 new Telefon {Nimetus = "Samsung Galaxy S22 Ultra", Tootja = "Samsung", Hind = "1349", Pilt = "samsung.jpg"},
                 new Telefon {Nimetus = "Xiaomi Mi 11 Lite 5G", Tootja = "Xiaomi", Hind = "399", Pilt = "xiaomi.jfif"},
                 new Telefon {Nimetus = "iPhone 13", Tootja = "Apple", Hind = "1179", Pilt = "iphone.jfif"},
                 new Telefon {Nimetus = "Xiaomi Note 10S pro", Tootja = "Xiaomi", Hind = "389", Pilt = "xiaomis.jfif"},
             };
+            var ruhmad = telefonid.GroupBy(p => p.Tootja).Select(g => new Ruhm<string, Telefon>(g.Key, g));
+            telefonideruhmades = new ObservableCollection<Ruhm<string, Telefon>>(ruhmad);
 
             //list = new ListView { ItemsSource = phones};
             //list.ItemSelected += List_ItemSelected;
             list = new ListView
             {
                 SeparatorColor = Color.Orange,
-                Header = "Minu oma kollektsioon:",
-                Footer = DateTime.Now.ToString("g"),
-
+                //Header = "Minu oma kollektsioon:",
+                Header = "Telefonid ruhmades",
+                //Footer = DateTime.Now.ToString("g"),
+                Footer = DateTime.Now.ToString("T"), 
                 HasUnevenRows = true,
-                ItemsSource = telefons,
+
+                ItemsSource = telefonideruhmades,
+                IsGroupingEnabled = true,
+                GroupHeaderTemplate = new DataTemplate(() =>
+                {
+                    Label tootja = new Label();
+                    tootja.SetBinding(Label.TextProperty, "Nimetus");
+                    return new ViewCell
+                    {
+                        View = new StackLayout
+                        {
+                            Padding = new Thickness(0, 5),
+                            Orientation = StackOrientation.Vertical,
+                            Children = { tootja }
+                        }
+                    };
+                }),
+                ItemTemplate = new DataTemplate(() =>
+                {
+                    Label nimetus = new Label { FontSize = 20 };
+                    nimetus.SetBinding(Label.TextProperty, "Nimetus");
+                    Label hind = new Label();
+                    hind.SetBinding(Label.TextProperty, "Hind");
+                    return new ViewCell
+                    {
+                        View = new StackLayout
+                        {
+                            Padding = new Thickness(0, 5),
+                            Orientation = StackOrientation.Vertical,
+                            Children = { nimetus, hind }
+                        }
+                    };
+                }),
+
+                //ItemsSource = telefons,
                 //ItemTemplate = new DataTemplate(()=>
                 //{
                 //    Label nimetus = new Label { FontSize = 20 };
@@ -65,15 +111,16 @@ namespace Listview
                 //        }
                 //    };
                 //})
-                ItemTemplate = new DataTemplate(() =>
-                {
-                    ImageCell imgcell = new ImageCell { TextColor = Color.Red, DetailColor = Color.Green };
-                    imgcell.SetBinding(ImageCell.TextProperty, "Nimetus");
-                    Binding companyBinding = new Binding { Path = "Tootja", StringFormat = "Tore telefon firmalt {0}" };
-                    imgcell.SetBinding(ImageCell.DetailProperty, companyBinding);
-                    imgcell.SetBinding(ImageCell.ImageSourceProperty, "Pilt");
-                    return imgcell;
-                })
+
+                //ItemTemplate = new DataTemplate(() =>
+                //{
+                //    ImageCell imgcell = new ImageCell { TextColor = Color.Red, DetailColor = Color.Green };
+                //    imgcell.SetBinding(ImageCell.TextProperty, "Nimetus");
+                //    Binding companyBinding = new Binding { Path = "Tootja", StringFormat = "Tore telefon firmalt {0}" };
+                //    imgcell.SetBinding(ImageCell.DetailProperty, companyBinding);
+                //    imgcell.SetBinding(ImageCell.ImageSourceProperty, "Pilt");
+                //    return imgcell;
+                //})
             };
             list.ItemTapped += List_ItemTapped;
 
@@ -98,14 +145,14 @@ namespace Listview
             Telefon phone = list.SelectedItem as Telefon;
             if(phone != null)
             {
-                telefons.Remove(phone);
+                //telefons.Remove(phone);
                 list.SelectedItem = null;
             }
         }
 
         private void Lisa_Clicked(object sender, EventArgs e)
         {
-            telefons.Add(new Telefon { Nimetus = "telefon", Tootja = "Tootja", Hind = "1" });
+            //telefons.Add(new Telefon { Nimetus = "telefon", Tootja = "Tootja", Hind = "1" });
         }
 
         private async void List_ItemTapped(object sender, ItemTappedEventArgs e)
---
+++ Listview/Listview/Ruhm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Listview
+{
+    public class Ruhm<K,T> : ObservableCollection<T>
+    {
+        public K Nimetus { get; private set; }
+        public Ruhm(K nimetus, IEnumerable<T> items)
+        {
+            Nimetus = nimetus;
+            foreach (T item in items)
+            {
+                Items.Add(item);
+            }
+        }
+    }
+}
